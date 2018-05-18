05baede5c53d5f1b84fc18707d78db47633c0846
Gonzalo B <gonzalobusnadiego@gmail.com>
2018-05-18 11:45:08 -0300

    Add Icon and force HTTPS

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,22 @@
 $(function () {
     var isIOS = /iPad|iPhone|iPod/.test(navigator.platform);
 
-    if (isIOS) {
+    // if (isIOS) {
 
-        var canvasVideo = new CanvasVideoPlayer({
-            videoSelector: '.video',
-            canvasSelector: '.canvas',
-            timelineSelector: false,
-            autoplay: true,
-            makeLoop: true,
-            pauseOnClick: false,
-            audio: false
-        });
+    //     var canvasVideo = new CanvasVideoPlayer({
+    //         videoSelector: '.video',
+    //         canvasSelector: '.canvas',
+    //         timelineSelector: false,
+    //         autoplay: true,
+    //         makeLoop: true,
+    //         pauseOnClick: false,
+    //         audio: false
+    //     });
 
-    }else {
-        // Use HTML5 video
-        document.querySelectorAll('.canvas')[0].style.display = 'none';
-    }
+    // }else {
+    //     // Use HTML5 video
+    //     document.querySelectorAll('.canvas')[0].style.display = 'none';
+    // }
 
     // Fix Jobs on mobile
     var windowsize = $(window).width();
