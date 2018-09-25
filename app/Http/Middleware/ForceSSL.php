7c1e11e59217ab7e3c9e2f7263800fba74d92ba3
Gonzalo B <gonzalobusnadiego@gmail.com>
2018-09-25 12:07:17 -0300

    Force SSL

--- app/Http/Middleware/ForceSSL.php
+++ app/Http/Middleware/ForceSSL.php
@@ -15,8 +15,9 @@ class ForceSSL
      */
     public function handle($request, Closure $next)
     {
+        dd(env('APP_ENV'));
+
         if (!$request->secure() && in_array(env('APP_ENV'), ['stage', 'production'])) {
-            dd(redirect()->secure($request->getRequestUri()));
 
             return redirect()->secure($request->getRequestUri());
         }
