c63727895892789b8d60c6dc83295777a051baea
Gonzalo B <gonzalobusnadiego@gmail.com>
2020-01-22 10:28:13 -0300

    Fix Email

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -18,7 +18,7 @@ class MainController extends Controller
 //            ['email' => 'gonzalobusnadiego@gmail.com', 'name' => 'Gonzalo']
 //        ];
 
-        Mail::to(['email' => 'contacto@sagaz.com.ar', 'name' => 'Gonzalo Busnadiego'], 'Nuevo Contacto Sagaz')->send(new Contacto($info));
+        Mail::to('contacto@sagaz.com.ar', 'Nuevo Contacto Sagaz')->send(new Contacto($info));
 
         return response()->json(['success'=>'Got Simple Ajax Request.']);
     }
